6023970f50b9231ac05449f0637b6c468ed26418
coderneha772 <136950073+coderneha772@users.noreply.github.com>
2023-06-18 15:06:14 +0530

    Add files via upload

---
+++ extra/template.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+       <title> Techno Drones & Cameras  </title>
+	   <link rel="stylesheet" type="text/css" href="drone.css">
+	   
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+crossorigin="anonymous">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="
+https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
+</head>
+<body>
+
+<header>
+<nav class="navbar navbar-expand-lg navbar-light bg-white px-4 border-bottom">
+  <div class="container-fluid">
+    <a class="navbar-brand fs-3" href="#">Techno <span class="text-primary"> Drones & Cameras </span> </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-5 text-center">
+        <li class="nav-item">
+          <a class="nav-link active text-primary" aria-current="page" href="template.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="drones.php">Drones</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+           
+            <li><a class="dropdown-item" href="drone%20installation.php">Drone Installation</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="drone%20working.php">Drone Working</a></li>
+          </ul>
+        </li>
+       <li class="nav-item">
+          <a class="nav-link" href="cameras.php">Cameras</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="contactus.php">Contact Us</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/first.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>   </h5>
+        <p>  </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/second.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>  </h5>
+        <p>   </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/third.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>  </h5>
+        <p>  </p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+</header>
+
+<footer class="text-center text-white" style="background-color: #4169e1">
+<!--Grid container-->
+<div class="container p-4 pb-0">
+   <!--Section Social media-->
+    <section class="mb-4">
+<img src="img/facebook.png" height="2%" width="2%">
+<img src="img/instagram.jpeg" height="2%" width="2%">
+<img src="img/twitter.png" height="2%" width="2%">
+<img src="img/google.png" height="2%" width="2%">
+</section>
+</div>
+         <div class="text-center p-3" style="background: rgba(0,0,0,0.2);">
+		 © 2023 Copyright:
+		 <a class="text-white" href="">2023 All Right Reserved by Bright Drones And cameras/TARUN BATHNOTRA </a>
+		 </div>
+</footer>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
